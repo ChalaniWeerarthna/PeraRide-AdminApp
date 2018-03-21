8054981afb9295e90a66cb5461d3af8c14fc879d
shyaman_madhawa <shayaman321@gmail.com>
2018-03-21 19:15:54 +0530

    new login interface

--- src/pages/LogInPage/LogInPage.jsx
+++ src/pages/LogInPage/LogInPage.jsx
@@ -1,8 +1,15 @@
-import React, {PropTypes} from 'react';  
-import TextInput from 'components/common/TextInput';  
+import React from 'react';  
 import {bindActionCreators} from 'redux';  
 import {connect} from 'react-redux';  
 import * as sessionActions from 'actions/sessionActions';
+import { Grid } from "material-ui";
+import {
+  RegularCard,
+  CustomInput,
+  ItemGrid,
+  Button
+} from "components";
+
 
 class LogInPage extends React.Component {  
   constructor(props) {
@@ -16,6 +23,8 @@ class LogInPage extends React.Component {
     const field = event.target.name;
     const credentials = this.state.credentials;
     credentials[field] = event.target.value;
+    console.log(event.target.name,event.target.value);
+    
     return this.setState({credentials: credentials});
   }
 
@@ -26,26 +35,65 @@ class LogInPage extends React.Component {
 
   render() {
     return (
-      < div>
-        < form>
-          < TextInput
-            name="email"
-            label="email"
-            value={this.state.credentials.email}
-            onChange={this.onChange}/>
-
-          < TextInput
-            name="password"
-            label="password"
-            type="password"
-            value={this.state.credentials.password}
-            onChange={this.onChange}/>
-
-          < input
-            type="submit"
-            className="btn btn-primary"
-            onClick={this.onSave}/>
-        </form>
+      < div >
+        <Grid 
+          container 
+          justify='center'
+          alignItems='center'
+          style={{height: 600}}>
+          <ItemGrid >
+            <RegularCard
+              cardTitle="Admin Login"
+              cardSubtitle="Enter your username and password"
+              content={
+                <div>
+                  <Grid container>
+                    <ItemGrid md={12}>
+                      <CustomInput
+                        labelText="Username"
+                        formControlProps={{
+                          fullWidth: true
+                        }}
+                        inputProps={{
+                          name: "email",
+                          value: this.state.credentials.email,
+                          onChange: this.onChange
+                        }}
+                      />
+                    </ItemGrid>
+                  </Grid>
+                  <Grid container>
+                    <ItemGrid md={12}>
+                      <CustomInput
+                        labelText="Password"
+                        formControlProps={{
+                          fullWidth: true
+                        }}
+                        inputProps={{
+                          name: "password",
+                          value: this.state.credentials.password,
+                          onChange: this.onChange,
+                          type: "password"
+                        }}
+                      />
+                    </ItemGrid>
+                  </Grid>
+                  <Grid container justify='center'>
+                    <ItemGrid md={6}>
+                      <Button
+                        fullWidth
+                        color="primary"
+                        onClick={this.onSave}
+                      >
+                        Login
+                      </Button>
+                    </ItemGrid>
+                  </Grid>
+                </div>
+              }
+            />
+          </ItemGrid>
+        </Grid>
       </div>
   );
   }
